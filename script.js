4cd4b41514f48f9c01c0ed533405c99c91a1aea3
Pamidimarri vijay sankar <sankerpamidimarri@gmail.com>
2024-11-21 14:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
+ let li=document.querySelector("#sampleTable");
+	
+ let len=document.getElementsByTagName("tr").length+1;
+ let v=document.createElement("tr");
+ let v1=document.createElement("td")
+	v1.textContent="Row"+len+"cell1";
+ let v2=document.createElement("td");
+	v2.textContent="Row"+len+"cell2";
+ v.append(v1,v2);
+ li.append(v);
   
 }
